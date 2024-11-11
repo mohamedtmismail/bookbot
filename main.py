ebfa3e42e79e6024d5f9d76236588809346e9d03
Mohamed Ismail <mohamed.tm.ismail@gmail.com>
2024-11-11 00:35:14 +0000

    added sorted list code

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(text)
     nmb_words = get_number_of_words(text)
-    print(f"Number of words: {nmb_words}")
     char_count = count_characters(text)
-    print(f"Character counts:\n {char_count}")
+    chars_sorted_list = chars_dict_to_sorted_list(char_count)
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"Number of words: {nmb_words}\n")
+    
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item['num']} times")
+
+    print("--- End report ---")
 
 
 def get_book_text(path):
@@ -27,6 +34,17 @@ def count_characters(text):
         else:
             chartocount[char] = 1
     return chartocount
+
+def sort_on(d):
+    return d["num"]
+
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
 
 
 main()
